945bbdd2dc32efeb44c1d4cdc1866d5cc71f30ca
Billy <billy_mckinnon@hotmail.com>
2021-02-06 05:07:32 +0000

    Added Placeholders

---
+++ db/seeds/02_poutines.sql
@@ -0,0 +1,2 @@
+INSERT INTO food (name, description, price, image URL)
+VALUES (Perogy, Fried perogies, double-smoked bacon, sour cream. 1450 CAL, $12.99, /Users/Billy/lighthouse/Vagrant/w6/midterm/midterm-resource-wall/public/images/Perogy-Poutine.png)
