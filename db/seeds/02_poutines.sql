0417a61d647122b2639e5aa833aa08ad36496f85
Billy McKinnon <billy_mckinnon@hotmail.com>
2021-02-07 11:32:46 -0800

    gittin ready for the day

--- db/seeds/02_poutines.sql
+++ db/seeds/02_poutines.sql
@@ -1,5 +1,12 @@
 INSERT INTO menu (name, description, price, imgURL)
-VALUES ('Perogy', 'Fried perogies, double-smoked bacon, sour cream. 1450 CAL', 1299, 'public/images/Perogy-Poutine.png'),
+VALUES
+('Traditional', 'Fresh hand-cut french fries, signature gravy, cheese curd. 1190 CAL', 1099, '/public/images/Traditional.png'),
+('Perogy', 'Fried perogies, double-smoked bacon, sour cream. 1450 CAL', 1299, 'public/images/Perogy-Poutine.png'),
 ('PopCorn Chicken', 'Fresh hand cut fries, cheese curd, signature gravy, popcorn chicken. 1230 CAL', 1299, 'public/images/popcorn-chicken.png'),
 ('Butter Chicken', 'Grilled chicken, butter chicken sauce, green onions. 1350 CAL', 1299, 'public/images/ButterChicken.png'),
-('Avalance', 'Fresh hand-cut French fries, Smoke’s Signature Gravy, Quebec cheese curds, chipotle pulled pork, grilled chicken, double-smoked bacon, and ranch. 1650 CAL', 1599, 'public/images/Avalanche_Poutine-thumb.png');
+('Avalance', 'Fresh hand-cut french fries, signature gravy, cheese curds, chipotle pulled pork, grilled chicken, double-smoked bacon, and ranch. 1650 CAL', 1599, 'public/images/Avalanche_Poutine-thumb.png'),
+('Tots On Top', 'Fresh hand-cut french fries, cheese curds, signature gravy, loaded with Tater Tots on top. 1350 CAL', 1299, '/public/images/TaterTot-thumb.png'),
+('Pogo', 'Fresh hand-cut french fries, cheese curd, signature gravy, chopped pogo. 1190 CAL', 1199, '/public/images/pogo-thumb.png'),
+('Veggie Rainbow Poutine', 'Fresh hand-cut french fries, veggie gravy, cheese curds, sriracha, guacamole. 1000 CAL', 1299, '/public/images/Rainbow-Poutine.png'),
+('Bacon CheeseBurger', 'Double-smoked bacon, prime ground beef, cheese sauce. 1400 CAL', 1399, '/public/images/Bacon-Cheeseburger-Poutine.png'),
+('Pulled Pork', 'Fresh hand-cut french fries, chipotle pulled pork 1360 CAL', 1399, 'public/images/Pulled-Pork-Poutine.png');
