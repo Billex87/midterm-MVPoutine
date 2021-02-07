20684cd9071ab736abc40cf444c2764008bf11f5
Billy McKinnon <billy_mckinnon@hotmail.com>
2021-02-07 11:45:43 -0800

    added

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, phone, email) VALUES ('Idil', 123, 'idil@idil.com');
-INSERT INTO users (name, phone, email) VALUES ('Sam', 456, 'sam@sam.com');
-INSERT INTO users (name, phone, email) VALUES ('Billy', 789, 'billy@billy.com');
+INSERT INTO users (name, phone, email) VALUES ('Idil', +1-111-1111, 'idil@jeilani.com');
+INSERT INTO users (name, phone, email) VALUES ('Sam', +1-222-2222, 'sam@anderson.com');
+INSERT INTO users (name, phone, email) VALUES ('Billy', +1-333-3333, 'billy@mckinnon.com');
+INSERT INTO users (name, phone, email) VALUES ('Tom Brady', +1-111-1111, 'goat@brady.com');
+INSERT INTO users (name, phone, email) VALUES ('Beyoncé Knowles', +1-222-2222, 'queen@bee.com');
+INSERT INTO users (name, phone, email) VALUES ('Kobe Bryant', +1-333-3333, 'mamba@mentality.com');
