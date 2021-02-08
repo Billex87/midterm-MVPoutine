5278a8e6e04296f12fd497448a0ae69e0afd09e9
Billy McKinnon <billy_mckinnon@hotmail.com>
2021-02-08 15:03:54 -0800

    order button

--- public/scripts/buttonorders.js
+++ public/scripts/buttonorders.js
@@ -1,11 +1,22 @@
 $(document).ready( () => {
-  $('.order-button').click(() => {
-    console.log('hello');
-    $.ajax({
-      method: "POST",
-      url: "/api/orders/"
-    }).done((res) => {
-      console.log('Add item to order');
-    });
+  $('.order-button').click(function() {
+    const button = $(this);
+    const div = button.parent()
+    const container = div.parent()
+    const p = container.find("p")
+    const price = container.find(".foot").find("p").text()
+    console.log("THE P", p.html());
+    console.log("Price", price);
+    cartArr.push({name:p.html(), price:price});
+    console.log(cartArr);
+
+    // $.ajax({
+    //   method: "POST",
+    //   url: "/api/orders/"
+    // }).done((res) => {
+    //   console.log('Add item to order');
+    // })
   })
-})
+
+  let cartArr = [];
+});
