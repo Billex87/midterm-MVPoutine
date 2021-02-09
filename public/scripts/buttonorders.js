e3a3d876c8e221676e0f2bca056cc6c00807a70a
Billy McKinnon <billy_mckinnon@hotmail.com>
2021-02-08 20:32:06 -0800

    nav

--- public/scripts/buttonorders.js
+++ public/scripts/buttonorders.js
@@ -1,13 +1,13 @@
-$(document).ready( () => {
+$(document).ready(() => {
   $('.order-button').click(function() {
     const button = $(this);
-    const div = button.parent()
-    const container = div.parent()
-    const p = container.find("p")
-    const price = container.find(".foot").find("p").text()
+    const div = button.parent();
+    const container = div.parent();
+    const p = container.find("p");
+    const price = container.find(".foot").find("p").text();
     // console.log("THE P", p.html());
     // console.log("Price", price);
-    cartArr.push({name:p.html(), price:price});
+    cartArr.push({ name: p.html(), price: price });
     console.log(cartArr);
     const newCartItem = createCartItem(p.html(), price);
     renderCart(newCartItem);
@@ -18,31 +18,30 @@ $(document).ready( () => {
     // }).done((res) => {
     //   console.log('Add item to order');
     // })
-  })
+  });
 
   let cartArr = [];
-//number of items function?
-//work on the order button
+  //number of items function?
+  //work on the order button
 
 
 
+  //this is all within the document ready
 
- //this is all within the document ready
-
- const createCartItem = function(name, price) {
-   const $cart= $(`<li>${name}</li>
+  const createCartItem = function(name, price) {
+    const $cart = $(`<li>${name}</li>
    <li>Price: ${price}</li>
    <li>Quantity: 1</li>
    `);
-     return $cart;
- }
+    return $cart;
+  };
 
- const renderCart = function(cartItems) {
-  const container = $(".nav-popup ul");
-  for (let item of cartItems) {
-    container.append(item);
-  }
-};
+  const renderCart = function(cartItems) {
+    const container = $(".nav-popup ul");
+    for (let item of cartItems) {
+      container.append(item);
+    }
+  };
 
 
 });
