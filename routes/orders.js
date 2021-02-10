0d5fb6c5fa0c72e792ed98266adb76e81c6a72aa
samjanderson <sam.andersonnn@live.ca>
2021-02-10 13:16:44 -0800

    changed orders.ejs and orders

--- routes/orders.js
+++ routes/orders.js
@@ -7,19 +7,19 @@ module.exports = (db) => {
   router.post("/", (req, res) => {
     // let totalPrice = document.getElementByID
     let totalPrice = 0;
-    console.log('req.body', req.body);
-    console.log('reqbodycart', req.body.cart)
+    // console.log('req.body', req.body);
+    // console.log('reqbodycart', req.body.cart)
     //calculate totalprice here
     const getTotalPrice = (cartArr) => {
       totalPrice = 0;
       for (let item of cartArr) {
         totalPrice += Number(item.price) * Number(item.quantity);
       }
-      console.log('totalPriceInside', totalPrice)
+      // console.log('totalPriceInside', totalPrice)
       return totalPrice;
     };
     console.log(getTotalPrice(req.body.cart))
-    console.log('totalPrice after', totalPrice)
+    // console.log('totalPrice after', totalPrice)
     db.query(`INSERT INTO orders (user_id, order_status, owner_id, total_price)
     VALUES (1, true, 1, ${totalPrice})
     RETURNING *;`)
@@ -37,9 +37,10 @@ module.exports = (db) => {
         }
 
         const orders = data.rows;
+        // console.log('orders', orders)
         twilio.smsOrderIn(data.rows[0].id, data.rows[0].total_price);
         twilio.smsRestaurant(data.rows[0].id)
-        res.render('orders', {orders});
+        res.redirect("/")
       })
       .catch(err => {
         console.log(err)
@@ -51,12 +52,36 @@ module.exports = (db) => {
   router.get("/", (req, res) => {
     // const ownerId = req.session.userID
     // console.log(ownerId)
-    db.query(`SELECT * FROM orders WHERE user_id = 1;`)
+    db.query(`SELECT *
+    FROM orders
+    JOIN order_items ON orders.id =  orders_id
+    JOIN menu ON menu.id = menu_id
+    WHERE user_id = 1
+    ;`)
       .then(data => {
         // console.log(data)
         const orders = data.rows;
+        let parsedData = {};
+        for (let item of orders) {
+          if (!parsedData[item.orders_id]) {
+            parsedData[item.orders_id] = []
+          }
+          parsedData[item.orders_id].push(item)
+        }
+        console.log('data.rows', data.rows)
+
+        const orderedData = Object.keys(parsedData).sort().reverse().reduce(
+          (obj, key) => {
+            obj[key] = parsedData[key];
+            return obj;
+          },
+          {}
+        );
+        console.log(orderedData)
+        // console.log('parsed data',parsedData)
+        // console.log('look for what I need', data.rows)
         // res.json({ orders });  // this line will crash the page- lets delete it
-        res.render('orders', {orders}); //added by idil to make orders page
+        res.render('orders', {orders: orderedData}); //added by idil to make orders page
         // console.log(orders)
       })
       .catch(err => {
