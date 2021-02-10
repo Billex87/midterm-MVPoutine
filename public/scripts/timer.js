cc3e1deb5e296c0d19cce035c4990b85f4f9e02c
Billy McKinnon <billy_mckinnon@hotmail.com>
2021-02-10 13:04:33 -0800

    yeee

--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -9,20 +9,19 @@ $(() => {
   const countdownElement = document.getElementById('countdown-timer');
 
   const updateCountdown = () => {
+    if ($(countdownElement).attr('data-status') === 'Done'){
+      clearInterval(intervalID)
+      return
+    }
     const minutes = Math.floor(time / 60);
     let seconds = time % 60;
-
     seconds = seconds < 10 ? '0' + seconds : seconds;
-
     countdownElement.innerHTML = `${minutes}:${seconds}`;
-
     time--;
     time = time < 0 ? 0 : time;
   };
 
-  setInterval(updateCountdown, 1000);
-
-
+  let intervalID = setInterval(updateCountdown, 1000);
 });
 
 
