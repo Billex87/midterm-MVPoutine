acd86086ed7bf4d11ba3f8507f881b0eab75da63
Billy McKinnon <billy_mckinnon@hotmail.com>
2021-02-10 09:10:21 -0800

    yeee

--- api/twilio.js
+++ api/twilio.js
@@ -1,27 +1,11 @@
-/*var client = require('twilio')(
-  'AC8cfb45b8582801f9a1e5d15bd2266e62',
-  '600a3635d5023f2dfe1f122da0e86485'
-);
-client.messages.create({
-  from: "+18016093070",
-  to: "+16477864414",
-  body: "Your order is ready for pickup!"
-}).then((message) => console.log(message.sid));
-client.messages.create({
-  from: "+18016093070",
-  to: "+19058308227",
-  body: "Your order is ready for pickup!"
-}).then((message) => console.log(message.sid));
-*/
 const accountSid = 'AC8cfb45b8582801f9a1e5d15bd2266e62'; //process.env.TWILIO_ACCOUNT_SID;
 const authToken  = '50b6c9f8bd27368b513fe4d8ee036755';  //process.env.TWILIO_AUTH_TOKEN;
 const client     = require('twilio')(accountSid, authToken);
 // POST REQUEST
-//add in total price here
+
 const smsOrderIn = (orderID, totalPrice) => {
-  const message = `CHA CHING. Order # is: ${orderID} and your total cost is: ${totalPrice} `
-  // console.log((order_items, "ORDER ITEMS"));
-  // console.log((order_items.keys));
+  const message = `Thanks, your order is #: ${orderID} and your total cost is: $${totalPrice}. Ready in 20 minutes or expect a text sooner. `
+
   client.messages.create({
     from: "+18016093070", //twilio
     to: "+17782146187",  //restaurant
@@ -46,19 +30,6 @@ const smsReady = () => {
     }
   });
 }
-// POST REQUEST
-// const smsCustomer = (orderid, phoneNumber) => {
-//   const message = `Hey There. Your Order ID From These Bad Ass Mofo's Is #${orderid} !`
-//   client.messages.create({
-//     from: "+18016093070", //twilio
-//     to: `+1${phoneNumber}`, //customer
-//     body: message
-//   }, (err, message) => {
-//     if (err) {
-//       return err;
-//     }
-//   });
-// }
 
 module.exports = {
   smsOrderIn,
