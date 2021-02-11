fc7f3b099dd06224ca0ea82cf58dc5f98efd10da
samjanderson <sam.andersonnn@live.ca>
2021-02-10 16:11:13 -0800

    added changes with Andy

--- routes/orders.js
+++ routes/orders.js
@@ -2,23 +2,24 @@ const express = require('express');
 const router = express.Router();
 const twilio = require('../api/twilio');
 
+
+const getTotalPrice = (cartArr) => {
+  let totalPrice = 0;
+  for (let item of cartArr) {
+    totalPrice += Number(item.price) * Number(item.quantity);
+  }
+  // console.log('totalPriceInside', totalPrice)
+  return totalPrice;
+};
+
 //order end** -30 min timer - order_end
 module.exports = (db) => {
   router.post("/", (req, res) => {
     // let totalPrice = document.getElementByID
-    let totalPrice = 0;
     // console.log('req.body', req.body);
     // console.log('reqbodycart', req.body.cart)
     //calculate totalprice here
-    const getTotalPrice = (cartArr) => {
-      totalPrice = 0;
-      for (let item of cartArr) {
-        totalPrice += Number(item.price) * Number(item.quantity);
-      }
-      // console.log('totalPriceInside', totalPrice)
-      return totalPrice;
-    };
-    console.log(getTotalPrice(req.body.cart))
+    const totalPrice = getTotalPrice(req.body.cart)
     // console.log('totalPrice after', totalPrice)
     db.query(`INSERT INTO orders (user_id, order_status, owner_id, total_price)
     VALUES (1, true, 1, ${totalPrice})
@@ -28,7 +29,7 @@ module.exports = (db) => {
         let cartItems = req.body.cart;
         console.log('data.rows', data.rows); //to check orders_id
         for (let item of cartItems) {
-          totalPrice += item.price;
+          // totalPrice += item.price;
           let paramsArray = [Number(item.id), data.rows[0].id, Number(item.quantity)];
           console.log('paramsArray', paramsArray);
           db.query(`INSERT INTO order_items (menu_id, orders_id, quantity)
@@ -57,6 +58,7 @@ module.exports = (db) => {
     JOIN order_items ON orders.id =  orders_id
     JOIN menu ON menu.id = menu_id
     WHERE user_id = 1
+    ORDER BY orders.id DESC
     ;`)
       .then(data => {
         // console.log(data)
@@ -68,20 +70,35 @@ module.exports = (db) => {
           }
           parsedData[item.orders_id].push(item)
         }
-        console.log('data.rows', data.rows)
+        // console.log('data.rows', data.rows)
 
-        const orderedData = Object.keys(parsedData).sort().reverse().reduce(
-          (obj, key) => {
-            obj[key] = parsedData[key];
-            return obj;
-          },
-          {}
-        );
+        // const orderedData = Object.keys(parsedData).sort().reverse().reduce(
+        //   (obj, key) => {
+        //     obj[key] = parsedData[key];
+        //     return obj;
+        //   },
+        //   {}
+        // );
+        const orderedData = Object.keys(parsedData).sort(function(a, b) {
+          return Number(a) - Number(b);
+        }).reverse().map((orderID) => {
+            const menuItems = parsedData[orderID]
+            return {orderID , menuItems, totalPrice: getTotalPrice(menuItems) }
+        }
+        )
+            // const totalPrice = menuItems.reduce((acc, menuItem) => {
+            //  return acc + (menuItem.quantity * menuItem.price)
+            // }, 0)
+
+
+        // console.log('orderedData', orderedData)
         console.log(orderedData)
-        // console.log('parsed data',parsedData)
+        console.log(parsedData)
         // console.log('look for what I need', data.rows)
         // res.json({ orders });  // this line will crash the page- lets delete it
-        res.render('orders', {orders: orderedData}); //added by idil to make orders page
+        // console.log('data.rows', data.rows)
+        // res.render('orders', {orders: data.rows})
+        res.render('orders', {orders: orderedData});
         // console.log(orders)
       })
       .catch(err => {
